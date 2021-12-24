dfd346897e0484089f46d1e17f91d11cccd95181
MityaDv <mityadv@gmail.com>
2021-12-24 22:08:38 +0200

    add provider

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,26 +1,26 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HelloWorld from '../views/HelloWorld.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: HelloWorld,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
---
+++ frontend/src/views/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="loadData">Query</button>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+// import { Query } from '@/generated/graphql'
+import gql from 'graphql-tag' // @ is an alias to /src
+
+export default class HelloWorld extends Vue {
+  msg = ''
+  async loadData() {
+    const res = await this.$apollo.query({
+      query: gql`
+        query {
+          helloWorld
+        }
+      `,
+    })
+
+    this.msg = res.data.helloWorld || ''
+
+    // const data: Query['helloWorld'] = res?.data?.helloWorld
+  }
+}
+</script>
